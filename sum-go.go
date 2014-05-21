d639edab48d9f6049783078eb9df96f07787bd4a
raman <raman@google.com>
2014-05-21 09:15:54 -0700

    stable

---
+++ sum-go.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+	fmt.Println("Regular sum")
+	go sum(a, c)
+	z := <-c
+	fmt.Println(z)
+}
