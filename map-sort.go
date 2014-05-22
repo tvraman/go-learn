cc09befd03dd41c9c390ba1754905bd3d1c0697e
T. V. Raman <rmaan@google.com>
2014-05-22 13:45:47 -0700

    works

---
+++ map-sort.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tvraman/go-learn/utils"
+
+)
+
+// Exercise sortmap.SortedKeys
+func main() {
+	m := map[string]int{
+		"a": 100,
+		"b": 7,
+		"c": 11,
+	}
+
+	for key, value := range m {
+		fmt.Printf("%v:   %v\n", key, value)
+	}
+	fmt.Println("Sorted")
+	s :=  sortmap.SortedKeys(m)
+	for i := range s {
+		fmt.Printf ("%v: %v \n", s[i], m[s[i]] )
+	}
+}
